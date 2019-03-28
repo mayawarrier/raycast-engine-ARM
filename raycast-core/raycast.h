51fa5b26917cdff71849d4c78fedf807fb605bfe
Dhruv Warrier <dhruv.warrier89@gmail.com>
2019-03-28 19:33:04 -0400

    Moved RAY_ANGLE_INC to raycast.h, provided trig fns with degrees - sind, cosd, and tand compute sin, cos, tan given degree arguments - RAY_ANGLE_INC is needed outside raycast-core to update the player angle only in increments of RAY_ANGLE_INC so that we move a valid number of columns

--- raycast-core/raycast.h
+++ raycast-core/raycast.h
@@ -8,6 +8,11 @@
 #define SCREEN_SIZE_X 320
 #define SCREEN_SIZE_Y 240
 #define FOV 60.0
+#define RAY_ANGLE_INC (FOV/SCREEN_SIZE_X);
+
+#define sind(x) (sin((x) * M_PI / 180))
+#define cosd(x) (cos((x) * M_PI / 180))
+#define tand(x) (tan((x) * M_PI / 180))
 
 typedef struct point_unit_coords {
 	int x;
@@ -25,9 +30,9 @@ typedef struct slice_info {
 	// additional texture info can be inserted here later
 } slice_info;
 
-point find_closest_horizontal_wall_intersection();
+point find_closest_horizontal_wall_intersection(int playerX, int playerY, double player_angle);
 
-point find_closest_vertical_wall_intersection();
+point find_closest_vertical_wall_intersection(int playerX, int playerY, double player_angle);
 
 double find_closest_distance_to_wall();
 
