76966b9e0cc49383a96ad0340aed8882052310c7
Dhruv Warrier <dhruv.warrier89@gmail.com>
2019-03-29 06:17:42 -0400

    Renamed header macro so that it does not clash with MAP_DATA array

--- Map_Data.h
+++ Map_Data.h
@@ -1,9 +1,9 @@
-#ifndef MAP_DATA
-#define MAP_DATA
+#ifndef MAP_DATA_H
+#define MAP_DATA_H
 
 #define MAP_SIZE_X 64
 #define MAP_SIZE_Y 64
 
-extern int map_data[MAP_SIZE_X][MAP_SIZE_Y];
+extern volatile int MAP_DATA[MAP_SIZE_X][MAP_SIZE_Y];
  
 #endif
