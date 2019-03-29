9af10792eb747eddf6581fad61aa6ae7d3a2056a
Dhruv Warrier <dhruv.warrier89@gmail.com>
2019-03-28 20:41:23 -0400

    Changed signature of find_closest_distance_to_wall() and cast_ray() - find_closest_distance_to_wall takes in 2 point* of the horizontal and vertical intersections respectively - It computes the distances to each wall intersection, if they exist, and returns the closer distance. If no horiz intersection, it will return the distance to the vert intersection, and vice versa - If neither intersections exist, returns NAN
    - cast_ray() returns a slice_info* instead. This is because slice may
    potentially contain texture data later and it will be more efficient to
    dynamically allocate.
    
    - if x and y are out of map bounds, point and grid_point x = y = INT_MAX
    - if slice does not exist at a certain screen_column, slice_info->size =
    slice_info->location = INT_MAX
    
    - Rename player_angle to ray_angle in
    find_closest_horizontal_wall_intersection() and
    find_closest_vertical_wall_intersection()

--- raycast-core/raycast.h
+++ raycast-core/raycast.h
@@ -4,6 +4,7 @@
 
 #include <math.h>
 #include <stdbool.h>
+#include <limits.h>
 
 #define SCREEN_SIZE_X 320
 #define SCREEN_SIZE_Y 240
@@ -14,28 +15,32 @@
 #define cosd(x) (cos((x) * M_PI / 180))
 #define tand(x) (tan((x) * M_PI / 180))
 
+// if point is out of map bounds, x = y = INT_MAX
 typedef struct point_unit_coords {
 	int x;
 	int y;
 } point;
 
+// if grid point is out of map bounds, x = y = INT_MAX
 typedef struct point_grid_coords {
 	int x;
 	int y;
 } grid_point;
 
+// if slice does not exist at this location, size = location = INT_MAX
 typedef struct slice_info {
 	int size;
 	int location;
 	// additional texture info can be inserted here later
 } slice_info;
 
-point find_closest_horizontal_wall_intersection(int playerX, int playerY, double player_angle);
+point find_closest_horizontal_wall_intersection(int playerX, int playerY, double ray_angle);
 
-point find_closest_vertical_wall_intersection(int playerX, int playerY, double player_angle);
+point find_closest_vertical_wall_intersection(int playerX, int playerY, double ray_angle);
 
-double find_closest_distance_to_wall();
+// if no wall exists at this ray, returns NAN
+double find_closest_distance_to_wall(point* horiz_intersection, point* vert_intersection);
 
-slice_info cast_ray(int playerX, int playerY, double player_angle, int screen_column);
+slice_info* cast_ray(int playerX, int playerY, double player_angle, int screen_column);
 
 #endif // RAYCAST_H
